a4820831b2e8bff6ceb80708ab252c33607c18cb
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-10-30 23:35:02 -0600

    Agregar archivos de proyecto.

---
+++ NARAPruebaUI/Guia5Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace NARAPruebaUI
+{
+    public class Guia5Test
+    {
+        private IWebDriver driver;
+
+        public Guia5Test()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Crear_Producto()
+        {
+            // Navega a Google
+            driver.Navigate().GoToUrl("https://localhost:7250/ProductNARA/Create");
+            // Realiza alguna acción en la página, por ejemplo, verificar el título de la página
+            Assert.Equal("Create - UI_MVC", driver.Title);
+        }
+
+        public void Dispose()
+        {
+            // Cierra el navegador
+            driver.Quit();
+        }
+    }
+}
